de800a897429e8b7eda9688ee9b1087f59ae9a80
Kevin <kevin.kds80@gmail.com>
2025-01-06 14:06:28 -0300

    add products api

--- controllers/products.ts
+++ controllers/products.ts
@@ -1,28 +1,66 @@
+import { Database } from "@db/mongo";
 import { Ctx } from "../dtos/ctx.ts";
+import { AuthService } from "../services/auth.ts";
+import { ProductService } from "../services/products.ts";
 import { IController } from "./IController.ts";
 
 export class ProductsController implements IController {
-  constructor() {
+  private service: ProductService;
+  private authService: AuthService;
+  private MAX_LIMIT = 100;
+  private MIN_PAGE = 1;
+
+  constructor(cache: Cache, db: Database) {
+    this.service = new ProductService(db);
+    this.authService = new AuthService(cache, db);
   }
 
-  public async getAll(_ctx: Ctx) {
-    return { code: 200, data: { oi: "ola da camada de produto" } };
+  public async getAll(ctx: Ctx) {
+    const url = ctx.request.url;
+    let page = parseInt(url.searchParams.get("page") ?? "1");
+    let limit = parseInt(url.searchParams.get("limit") ?? "10");
+    page = page < this.MIN_PAGE ? this.MIN_PAGE : page;
+    limit = limit > this.MAX_LIMIT || limit < 1 ? this.MAX_LIMIT : limit;
+
+    const sessionToken = ctx.request.headers.get("SESSION_TOKEN") ?? "";
+    const userId = ctx.params.client_id;
+    if (!await this.authService.canAccessResource(sessionToken, userId)) {
+      return { code: 403, data: { error: "User dont have enough permission" } };
+    }
+    return await this.service.getProducts(userId, page, limit);
   }
 
   public async get(ctx: Ctx) {
-    console.log(ctx.params);
-    return { code: 200, data: { oi: "ola da camada de produto" } };
+    const sessionToken = ctx.request.headers.get("SESSION_TOKEN") ?? "";
+    const userId = ctx.params.client_id;
+    const productId = ctx.params.path_id;
+    if (!await this.authService.canAccessResource(sessionToken, userId)) {
+      return { code: 403, data: { error: "User dont have enough permission" } };
+    }
+    return await this.service.getProduct(userId, productId);
+  }
+
+  public async update(ctx: Ctx) {
+    const sessionToken = ctx.request.headers.get("SESSION_TOKEN") ?? "";
+    const userId = ctx.params.client_id;
+    const productId = ctx.params.path_id;
+    if (!await this.authService.canAccessResource(sessionToken, userId)) {
+      return { code: 403, data: { error: "User dont have enough permission" } };
+    }
+    return await this.service.addProduct(userId, productId);
   }
 
   public async save(_ctx: Ctx) {
-    return { code: 200, data: { oi: "ola da camada de produto" } };
+    return { code: 405, data: { error: "Method not allowed" } };
   }
 
-  public async update(_ctx: Ctx) {
-    return { code: 200, data: { oi: "ola da camada de produto" } };
-  }
-
-  public async delete(_ctx: Ctx) {
-    return { code: 200, data: { oi: "ola da camada de produto" } };
+  public async delete(ctx: Ctx) {
+    const sessionToken = ctx.request.headers.get("SESSION_TOKEN") ?? "";
+    const userId = ctx.params.client_id;
+    const productId = ctx.params.path_id;
+    if (!await this.authService.canAccessResource(sessionToken, userId)) {
+      return { code: 403, data: { error: "User dont have enough permission" } };
+    }
+    return await this.service.deleteProduct(productId);
   }
 }
---
+++ services/products.ts
@@ -0,0 +1,66 @@
+import { Collection, Database } from "@db/mongo";
+import { products, ProductSchema } from "../repositories/products/schema.ts";
+import { IntegrationProductsApi } from "../integrations/services/products-api.ts";
+
+export class ProductService {
+  private collection: Collection<ProductSchema>;
+  private productApi: IntegrationProductsApi;
+
+  constructor(db: Database) {
+    this.collection = products(db);
+    this.productApi = new IntegrationProductsApi();
+  }
+
+  public async getProducts(userId: string, page=1, limit=10) {
+    const products = await this.collection.find({
+      userId
+    })
+    .skip((page - 1) * limit)
+    .limit(limit)
+    .toArray();
+
+    return { code: 200, data: products };
+  }
+
+  public async getProduct(userId: string, productId: string) {
+    const product = await this.collection.findOne({userId, productId});
+
+    if (!product) {
+      return { code: 404, data: { error: "Product not found in db" } };
+    }
+
+    return { code: 200, data: product };
+  }
+
+  public async addProduct(userId: string, productId: string) {
+    const { code } = await this.getProduct(userId, productId);
+    if (code == 200) {
+      return { code: 409, data: { error: "Product already in list" } };
+    }
+
+    const product = await this.productApi.getOneProduct(productId);
+    if (!product) {
+      return {
+        code: 404,
+        data: { error: "Unable to find product in products api" },
+      };
+    }
+    
+    this.collection.insertOne({
+        userId,
+        productId,
+        image: product.image,
+        price: product.price,
+        title: product.title,
+        review: product.reviewScore
+    } as ProductSchema)
+    return { code: 204, data: undefined };
+  }
+
+  public async deleteProduct(productId: string) {
+    await this.collection.deleteOne(
+      { productId },
+    );
+    return { code: 204, data: undefined };
+  }
+}
